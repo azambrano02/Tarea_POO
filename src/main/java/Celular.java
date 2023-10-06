04fb8654a9b68f96ce8cce5b7ebd397a30c8a2cf
azambrao02 <a.zambrano02@ufromail.cl>
2023-10-06 02:17:48 -0300

    clase celular con los metodos agregados

--- src/main/java/Celular.java
+++ src/main/java/Celular.java
@@ -7,5 +7,21 @@ public class Celular {
     String procesador;
     String compañiaTelefonica;
 
+    public void llamar(){
+        // un metodo que te permita llamar a otros "celulares"
+    }
+    public void tomarFotos(){
+        // un meotodo para tomar fotografias y almacenarlas en "almacenamiento"
+        // si guarda la foto no deberia ser un void, pero no se que clase deberia ser
+    }
+    public void verHora(){
+        // un metodo que te muestre la hora mediante un print
+    }
+    public int descargarAplicaciones(){
+        // un metodo para descargar aplicaciones de la web
+    }
+    public void reproducirMusica(){
+        // un metodo para reproducir musica almacenada en el celular
+    }
 
 }
