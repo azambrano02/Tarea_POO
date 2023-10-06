3a789f0e98ff3f3e54c744d6461e0d7768d10add
azambrao02 <a.zambrano02@ufromail.cl>
2023-10-06 02:13:04 -0300

    clase celular con sus atributos

---
+++ src/main/java/Celular.java
@@ -0,0 +1,11 @@
+public class Celular {
+    String marca;
+    int almacenamiento;
+    int camaras;
+    int precio;
+    String color;
+    String procesador;
+    String compañiaTelefonica;
+
+
+}
